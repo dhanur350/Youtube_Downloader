37f89afccebb3baf2f43eaacc6c5eb6fa5bd3d5c
dhanur350 <dhanur297@gmail.com>
2022-11-07 18:58:51 +0530

    Day3.1

--- src/youtube_downloader/YoutubeUI.java
+++ src/youtube_downloader/YoutubeUI.java
@@ -11,8 +11,23 @@ import javax.swing.*;
  * @author Dhanur
  */
 public class YoutubeUI {
+//Components of UI
+
+    JTextField urlLinkbox, downloadLoc;
+    JLabel urlVideo, Location;
 
     YoutubeUI() {
+        JFrame frame = new JFrame();
+        urlLinkbox = new JTextField("Enter your URL", 10);
+        frame.add(urlLinkbox);
+        frame.setSize(800, 500);
+        frame.setLayout(null);
+        frame.setVisible(true);
     }
+    //Main Function
 
+    public static void main(String[] args) {
+        //Direct Call the constructor
+        new YoutubeUI();
+    }
 }
